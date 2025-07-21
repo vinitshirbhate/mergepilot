a44d053020387eeee8dd370d521746cfda421f08
vinitshirbhate <vinit.shirbhate23@vit.edu>
2025-07-21 20:29:35 +0530

    Styles:Style changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,17 +11,12 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist-sans",
-});
-
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className={`${geist.variable}`}>
+      <html lang="en">
         <body>
           <TRPCReactProvider>{children}</TRPCReactProvider>
         </body>
